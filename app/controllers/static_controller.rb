4be5f47acfc713a587ed317b83bcca257156bd01
Nikky Southerland <nikky@uw.edu>
2013-04-08 21:04:11 -0700

    Delete it alll

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def about
+  end
+
+  def contact
+  end
+
+  def license
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+YetAnotherMonitoringProgram::Application.config.session_store :cookie_store, key: '_Yet-Another-Monitoring-Program_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# YetAnotherMonitoringProgram::Application.config.session_store :active_record_store
