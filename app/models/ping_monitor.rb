ecf02b58388449805b9a87c1ad41790798744d0f
Nikky Southerland <nikky@uw.edu>
2013-04-14 14:02:59 -0700

    Add to_s for PingMonitor; Fix attr_accessible

--- app/models/ping_monitor.rb
+++ app/models/ping_monitor.rb
@@ -1,7 +1,14 @@
 class PingMonitor < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :hostname
   has_one :BaseMonitor, as: :monitorable
   def type
   	"Ping Monitor"
+  end
+  def to_s
+  	if self.hostname
+  		"Ping Monitor for #{self.hostname}"
+  	else
+  		"Ping Monitor for Undefined Host"
+  	end
   end
 end
