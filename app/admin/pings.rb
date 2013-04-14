9012a2fd010d3c6c8296fa006e09d6f3f492ab8b
Nikky Southerland <nikky@uw.edu>
2013-04-14 10:41:20 -0700

    Add belongs_to user(ping)

--- app/admin/pings.rb
+++ app/admin/pings.rb
@@ -1,3 +1,2 @@
 ActiveAdmin.register Ping do
-  
 end
--- app/models/ping.rb
+++ app/models/ping.rb
@@ -1,6 +1,7 @@
 class Ping < ActiveRecord::Base
   attr_accessible :frequency_in_seconds, :hostname
   has_many :PingResults
+  belongs_to :user
   def to_s
   	self.hostname
   end
