80d565f44cf9cbc0b36b3a5f42019cc0c8283769
Nikky Southerland <nikky@uw.edu>
2013-04-14 11:01:17 -0700

    Add to_s for alert

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,4 +2,7 @@ class Alert < ActiveRecord::Base
   attr_accessible :active, :ping_id, :user_id
   belongs_to :ping
   belongs_to :user
+  def to_s
+  	"Alert for #{self.ping.to_s} owned by #{self.user.to_s}"
+  end
 end
