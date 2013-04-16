0bc52907d5d5aff547967fca28be2a47547bdc2b
Nikky Southerland <nikky@uw.edu>
2013-04-15 19:48:18 -0700

    Add exception string to WebResult

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130415195852) do
+ActiveRecord::Schema.define(:version => 20130416192510) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(:version => 20130415195852) do
   add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
   add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
-  create_table "alerts", :force => true do |t|
-    t.boolean  "active"
-    t.integer  "user_id"
-    t.integer  "ping_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
   create_table "base_monitors", :force => true do |t|
     t.string   "name"
@@ -66,14 +58,6 @@ ActiveRecord::Schema.define(:version => 20130415195852) do
     t.integer  "ping_monitor_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
-  end
-
-  create_table "pings", :force => true do |t|
-    t.string   "hostname"
-    t.integer  "frequency_in_seconds"
-    t.integer  "user_id"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
   end
 
   create_table "users", :force => true do |t|
@@ -108,6 +92,7 @@ ActiveRecord::Schema.define(:version => 20130415195852) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.string   "web_monitor_id"
+    t.string   "exception"
   end
 
 end
