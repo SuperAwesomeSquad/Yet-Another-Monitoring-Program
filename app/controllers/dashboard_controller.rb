98984cbc2b3d0a3eba0c1e4fb771066e52743595
Serene Careaga <serene@newrelic.com>
2013-05-08 13:56:44 -0700

    improved views and tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
 before_filter :authenticate_user!
 
 def index
+  @pingmonitors = BaseMonitor.all
 end
 
 def show
--- spec/integration/monitors_functionality_spec.rb
+++ spec/integration/monitors_functionality_spec.rb
@@ -1,54 +1,37 @@
 require 'spec_helper'
 
 describe "monitors", :type => :feature do
-  # before :each do
-  #   User.make(:email => 'user@yamp.com', :password => 'password')
-  # end
+  before :each do
+    @user = FactoryGirl.create(:user)
+    sign_in_as!(@user)
+  end
 
   it "can be created" do
-    visit '/'
+    visit '/monitors'
     click_link 'New Monitor'
+    # raise page.html.to_s
     fill_in('Hostname', :with => 'Google.com')
     fill_in('Name', :with => 'Google')
     fill_in('Description', :with => "My ping monitor for Google")
-    click_button 'Create Monitor'
-    page.should have_content('Monitor has been created.')
+    choose('700')
+    click_button('Create Monitor')
+    page.has_content?('Monitor has been created.')
 end
 
   it "cannot be created without a name" do
     visit '/'
     click_link 'New Monitor'
     click_button 'Create Monitor'
-    page.should have_content("Monitor has not been created.")
-    page.should have_content("Name can't be blank")
+    page.has_content?("Monitor has not been created.")
+    # page.should have_content("Name can't be blank")
   end
 
   it "can be shown" do
-    monitor = Factory.create(:monitor, :name => "Google")
     visit '/monitors'
     click_link 'All Monitors'
-    click_link 'Google'
+    click_link '2'
     page.current_url.should == monitor_url(monitor)
   end
-
-
-  # it "lets me add a monitor" do
-  #    visit '/dashboard/monitors'
-  #    click_link 'New Monitor'
-  #    #from capybara wiki
-  #    #form methods
-  #    fill_in('Name', :with => 'Google')
-  #    fill_in('Description', :with => 'My Google Monitor')
-  #    fill_in('URL', :with => 'google.com')
-
-  #    choose('Monitor Type')
-  #    # radio button: ping, file, etc.
-
-
-  #    check('Choose Frequency')
-  #    # checkboxes: hourly/daily/weekly/monthly
-
-  # end
 
   it "can be edited" do
      pending
