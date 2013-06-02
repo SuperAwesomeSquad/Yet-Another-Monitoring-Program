02bfbabbc73fb447ab6912e1c6adf03d1c7716bd
Nikky Southerland <nikky@uw.edu>
2013-06-02 16:04:20 -0700

    Add initial alerts controller support

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -2,21 +2,21 @@ class AlertsController < ApplicationController
 before_filter :authenticate_user!
 
 def index
-  @alerts = current_user.alerts
+  @alerts = current_user.get_active_alerts
 end
 
 def show
   alert = Alert.find(params[:id])
-  if alert.user != current_user
+  if alert.user != current_user || alert.nil?
   	flash[:error] = "You do not have permission to view this alert"
   	redirect_to admin_dashboard_path
-  else
+  else 
   	@alert = alert
   end
 end
 
-def alerts
-    # @alerts = nil
-end
+# def alerts
+#     # @alerts = nil
+# end
 
 end
