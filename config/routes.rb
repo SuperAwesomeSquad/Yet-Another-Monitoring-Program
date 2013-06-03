460ab2210808e1b8301f03e749851e0e4fbde68f
Nikky Southerland <nikky@uw.edu>
2013-06-02 23:57:22 -0700

    Cleanup routes for dashboard
    Clean up and re-arrange routing for dashboard. If user signed in, treat root as dashboard#index; otherwise root is static#welcome. Additionally, remove namespacing. Dashboard referred to as simply root_path for logged_in users.
    
    Modify application layout to not show dashboard/alert links for non-logged in users.

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,16 @@ YetAnotherMonitoringProgram::Application.routes.draw do
 require 'sidekiq/web'
 
 devise_for :users
-root :to => 'static#welcome'
 
 %w[about contact license].each do |page|
   get page, controller: 'static', action: page
 end
 
-# get '/dashboard',
-#   :to => 'dashboard#index',
-#   :as => '/dashboard'
-
-namespace :dashboard do
-    get '', to: 'dashboard#index', as: '/'
+authenticated :user do
+  root :to => 'dashboard#index'
 end
 
-# match "dashboard" => "monitors#show"
+root :to => 'static#welcome'
 
 resources :monitors
 
@@ -26,12 +21,10 @@ get '/lsp',
   :to => 'lumpys#index',
   :as => 'lsp'
 
-authenticated :user do
-  root :to => "dashboard#index"
-end
-
 ActiveAdmin.routes(self)
 
-mount Sidekiq::Web, at: '/sidekiq'
+authenticate :user do
+  mount Sidekiq::Web, at: '/sidekiq'
+end
 
 end
