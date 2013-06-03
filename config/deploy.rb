66b823197289e95bd4457977594d4ab3735e9608
Nikky Southerland <nikky@uw.edu>
2013-06-02 23:47:54 -0700

    Set up to use capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,16 @@
+require 'sidekiq/capistrano'
+require 'bundler/capistrano'
+require "rvm/capistrano"
+
 set :application, "YAMP"
 set :repository,  "git://github.com/SuperAwesomeSquad/Yet-Another-Monitoring-Program.git"
 set :scm, :git
+set :branch, "master"
+set :deploy_via, :remote_cache
+
 set :deploy_to, "/home/yamp/yamp"
 
+set :ssh_options, { :forward_agent => true }
 
 role :web, "sylvan.nykida.net"
 role :db, "sylvan.nykida.net", :primary => true
@@ -11,20 +19,29 @@ role :app, "sylvan.nykida.net"
 set :user, "yamp"
 set :use_sudo, false
 
+namespace(:custom) do
+  task :symlink_db, :roles => :app do
+  run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
 
+#after "deploy:update_code", "custom:symlink_db"
+before "deploy:assets:precompile", "custom:symlink_db"
 
+namespace :deploy do
+  desc "Start Thin"
+  task :start do
+    run "cd /home/yamp/yamp/current; bundle exec thin start -C yamp_prod.yml"
+  end
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+  desc "Stop Thin"
+  task :stop do
+    run "cd /home/yamp/yamp/current; bundle exec thin stop -C yamp_prod.yml"
+  end
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+  desc "Restart thin"
+  task :restart do
+    run "cd /home/yamp/yamp/current; bundle exec thin restart -C yamp_prod.yml"
+  end
+end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
